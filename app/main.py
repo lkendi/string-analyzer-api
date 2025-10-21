834c16772a459bffa3567446eea9f745e36ca036
lkendi <lkendi003@gmail.com>
2025-10-21 18:38:43 +0300

    feat: Implement string filtering in the GET /strings endpoint based on query parameters

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 """API routes module"""
 from hashlib import sha256
 from datetime import datetime
-from fastapi import FastAPI, HTTPException, status
-from app.schemas.string import StringRequest, StringResponse, StringProperties
+from fastapi import FastAPI, HTTPException, status, Depends
+from app.schemas.string import StringRequest, \
+                                StringResponse, \
+                                StringProperties, \
+                                StringFilters, \
+                                StringListResponse
 from app.storage import strings_db
 
 
@@ -21,11 +25,11 @@ def root():
     return {"message": "Hello! String API is running."}
 
 
-@app.get("/strings", response_model=list[StringResponse])
-def get_strings():
+@app.get("/strings", response_model=StringListResponse)
+def get_all_strings(filters: StringFilters = Depends()):
 
     """
-    Get all strings
+    Get all strings with optional filters
 
     Returns
     -------
@@ -33,14 +37,65 @@ def get_strings():
 
     Raises
     ------
-    HTTPException - if no strings exist
+    HTTPException - if no strings exist or if the query parameters are invalid
     """
+
+    try:
+        if filters.contains_character and len(
+                filters.contains_character) > 1:
+            raise ValueError("contains_character must be a single character.")
+        if filters.min_length is not None and filters.min_length < 0:
+            raise ValueError("min_length must be a positive integer.")
+        if filters.max_length is not None and filters.max_length < 0:
+            raise ValueError("max_length must be a positive integer.")
+        if filters.word_count is not None and filters.word_count < 0:
+            raise ValueError("word_count must be a positive integer.")
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Invalid query parameter values or types: {str(e)}"
+        ) from e
+
     if not strings_db:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="No strings found."
         )
-    return list(strings_db.values())
+
+    results = list(strings_db.values())
+
+    if filters.is_palindrome is not None:
+        results = list(filter(
+            lambda x: x.properties.is_palindrome ==
+            filters.is_palindrome, results))
+
+    if filters.min_length is not None:
+        results = list(filter(
+            lambda x: x.properties.length >= filters.min_length, results))
+
+    if filters.max_length is not None:
+        results = list(filter(
+            lambda x: x.properties.length <= filters.max_length, results))
+
+    if filters.word_count is not None:
+        results = list(filter(
+            lambda x: x.properties.word_count == filters.word_count, results))
+
+    if filters.contains_character:
+        results = list(filter(
+            lambda x: filters.contains_character in x.value, results))
+
+    return {
+        "data": results,
+        "count": len(results),
+        "filters_applied": {
+            "is_palindrome": filters.is_palindrome,
+            "min_length": filters.min_length,
+            "max_length": filters.max_length,
+            "word_count": filters.word_count,
+            "contains_character": filters.contains_character
+        }
+    }
 
 
 @app.get("/strings/{string_value}", response_model=StringResponse)
--- app/schemas/string.py
+++ app/schemas/string.py
@@ -1,6 +1,7 @@
 """String schema module"""
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional, List
 
 class StringProperties(BaseModel):
     """
@@ -12,6 +13,16 @@ class StringProperties(BaseModel):
     word_count: int
     sha256_hash: str
     character_frequency_map: dict
+
+class StringFilters(BaseModel):
+    """
+    Filters for string requests
+    """
+    is_palindrome: Optional[bool] = None
+    min_length: Optional[int] = None
+    max_length: Optional[int] = None
+    word_count: Optional[int] = None
+    contains_character: Optional[str] = None
 
 
 class StringRequest(BaseModel):
@@ -29,3 +40,11 @@ class StringResponse(BaseModel):
     value: str
     properties: StringProperties
     created_at: datetime
+
+class StringListResponse(BaseModel):
+    """
+    Format of a list of string responses
+    """
+    data: List[StringResponse]
+    count: int
+    filters_applied: StringFilters
